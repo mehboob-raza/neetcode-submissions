b5d0bb0e822e3eb1bb12717f2ed0eb02dd27a70a
Mehboob Raza <98275465+mehboob-raza@users.noreply.github.com>
2026-07-26 06:08:04 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if(s.length !== t.length){
+            return false
+        }
+        const sortedS = s.split("").sort().join("")
+        const sortedT = t.split("").sort().join("")
+
+        return sortedS === sortedT    
+    }
+}
+
+const sol = new Solution()
+console.log(sol.isAnagram("racecar", "carrace" ))
